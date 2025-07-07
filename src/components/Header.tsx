ffd2cb2e8ac5a2ac77b0553830bc09891efe03fb
KIK48 <kikeperezalvarez@gmail.com>
2025-07-06 23:22:42 -0600

    Changed the size of the navbar and made it more to the right side. I also made the search bar skinnier. Added the logo using svg. Donate button got moved all the way to the side like a tab and added a hover effect that shows the letters and changes the background color to a lighter green and expands the button to the left once not hovering it moves back automatically to the right like before

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,25 @@
+import logo from '../assets/one-world-logo-3Colors.svg';
+
 function Header() {
     return (
         <>
+        <div className = 'Header'>
             <div className = "top-header">
                 <div className = "logo-space">
-                    <h1>Logo goes here</h1> {/* this is a place holder for the logo */}
+                    <img src = {logo} alt = 'OWQ-Logo' className = 'Logo_OWQ'/>
                 </div>
                 <div className = "searchbar-space">
                     <h1>Search Pages...</h1> {/* this is a place holder for the searchbar component */}
                 </div>
-                <div className = "donate-space">
-                    <h1>Donate</h1> {/* this is a place holder for the donate component button */}
-                </div>
+                <button className = "donate-space">
+                    <h1 className='Donate-text'>Donate</h1> {/* this is a place holder for the donate component button */}
+                </button>
                 
             </div>
             <div className = "bottom-header">
                 <div className="nav">Navbar</div>
             </div>
-            
+        </div>
         </>
     )
 }

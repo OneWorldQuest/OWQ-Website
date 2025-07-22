d9b12e1d3661118abc01c960b140909261236be4
KIK48 <kikeperezalvarez@gmail.com>
2025-07-22 00:38:41 -0600

    Added the NavLinks which help do routing and also added something to keep track in which page we are on in the navbar. When in active page the text is going to be gold. So by default it should be home in gold.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,6 +9,7 @@
 
 import logo from '../assets/one-world-logo-3Colors.svg';
 import '../styles/Header.css'
+import {NavLink} from 'react-router-dom'
 
 function Header() {
 
@@ -25,7 +26,7 @@ function Header() {
                 <div className = "donate-border">
                     <button className='donate-button'>
                         <div className="donate-background">
-                            <h1 className="donate-text">DONATE</h1>
+                            <h1 className="donate-text">donate</h1>
                         </div>
                     </button>
                 </div>
@@ -35,10 +36,10 @@ function Header() {
             <div className = "bottom-header">
                 <div className="nav">
                     <ul>
-                        <li><a href='#'>Home</a></li>
-                        <li><a href='#'>About Us</a></li>
-                        <li><a href='#'>Programs</a></li>
-                        <li><a href='#'>Contact Us</a></li>
+                        <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Home</NavLink>
+                        <NavLink to="/about" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>About Us</NavLink>
+                        <NavLink to="/programs" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Programs</NavLink>
+                        <NavLink to="/get-involved" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Get Involved</NavLink>
                     </ul>
                 </div>
             </div>

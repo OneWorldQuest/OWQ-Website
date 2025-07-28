a2355be7a7987444340c40dce11d46502b279abf
Vijay-Kumar-Reddy <vijaymarripati7@gmail.com>
2025-07-28 19:02:10 -0400

    Created ImageTextBox

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -8,11 +8,22 @@
  */
 
 import './../styles/AboutUs.css'
+import ImageTextBox from '../components/ImageTextBox'
+import TeachersTeachingToStuents from './../assets/teachers-teaching-to-students.png'
 
 function AboutUs() {
     return (
         <>
-            <p>This is the About Us page content file</p>
+            <div className='AboutUsBody' style={{}}>
+
+                <ImageTextBox
+                    image={TeachersTeachingToStuents}
+                    text ="  Phasellus tincidunt mattis fringilla. Phasellus mollis nisi a tortor commodo laoreet. 
+    Maecenas at feugiat risus. Suspendisse eget enim nulla. Etiam vehicula condimentum lacus id posuere. 
+    Morbi dignissim at ligula eget interdum. Nam non vulputate leo, quis gravida nibh. 
+    Donec gravida erat ac rutrum tincidunt."
+                    />                
+            </div>
         </>
     )
 }

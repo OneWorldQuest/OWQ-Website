c4cd97bf3a4468bef0434da070a0676cf6fb93fc
KIK48 <kikeperezalvarez@gmail.com>
2025-08-06 15:10:50 -0600

    Laptop not charging anymore and I have to move to my pc

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -8,13 +8,30 @@
 //  */
 
 import '../styles/Searchbar.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Searchbar() {
+
+const [query, setQuery] = useState('');
+const navigate = useNavigate();
+
+const pages = [
+    {name: 'Home', path: '/'},
+    {name: 'About US', path: '/about'},
+    {name: 'Get Involved', path: '/get-involved'},
+    {name: 'Programs', path: '/programs'}
+];
+
+const filteredPages = pages.filter(page => page.name.toLowerCase().includes(query.toLowerCase()));
+
     return (
         <input
         type='text'
         className='search-input'
         placeholder='Search Pages'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
         />
     )
 }
